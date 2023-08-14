36db8d95c99e2cb89d422fad1bfa4fa12eda5da6
Stranger29 <140977875+shahadat14@users.noreply.github.com>
2023-08-14 07:08:47 +0600

    Update bubble sort .c

--- bubble sort .c
+++ bubble sort .c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+int main(){
+    int n ;
+    scanf ("%d",&n);
+    int array[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(array[j]>array[j+1]){
+                int temp = array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+    printf("After sorted\n");
+    for(int i=0;i<n;i++){
+        printf("%d",array[i]);
+    }
